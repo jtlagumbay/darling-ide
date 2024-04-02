0159d52ed0fbf7bd29081d6e86d73d822bda7bb6
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-04-02 11:52:10 +0800

    add new/open functionality

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -9,18 +9,94 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
 import ZoomOutIcon from '@mui/icons-material/ZoomOut';
 import { useCurrentEditor } from "@tiptap/react";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Menubar() {
   const { editor } = useCurrentEditor();
   const [zoomLevel, setZoomLevel] = useState(100); // Initial zoom level
+  const [unsavedChanges, setUnsavedChanges] = useState(false);
+  const fileInputRef = useRef(null);
+
+  /** New/Open File Functionalities **/
+  const handleNewFile = (event) => {
+    if (!unsavedChanges) {
+      // Clear the content of the editor
+      editor.commands.clearContent();
+    } else {
+      const confirmNewFile = window.confirm("There are unsaved changes. Are you sure you want to create a new file?");
+      if (confirmNewFile) {
+        // Clear the content of the editor
+        editor.commands.clearContent();
+        // Reset unsavedChanges state
+        setUnsavedChanges(false);
+      } 
+    }
+  }
+
+  const handleOpenFile = () => {
+    if (unsavedChanges) {
+      const confirmOpenFile = window.confirm("There are unsaved changes. Are you sure you want to open a new file?");
+      if (!confirmOpenFile) { 
+        return
+      }
+    } 
+    fileInputRef.current.click()
+  }
+  const openFileExplorer = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const fileContent = reader.result;
+      editor.commands.setContent(fileContent);
+    };
+
+    reader.readAsText(file);
+  };
+
+  const handleSave = () => {
+    
+  }
+
+  /** Save/As Functionalities **/
+  const handleSaveAs = () => {
+    // Get the HTML content of the editor
+    const content = editor.getText();
+    console.log(content)
+    // Create a Blob object containing the HTML content
+    const blob = new Blob([content], { type: 'text/html' });
+
+    // Create a temporary anchor element to trigger the download
+    const anchor = document.createElement('a');
+    anchor.href = URL.createObjectURL(blob);
+    anchor.download = 'document.txt';
+    
+     // Listen for the download start event
+    anchor.addEventListener('click', () => {
+      // Update unsavedChanges only if the download starts
+      setUnsavedChanges(false);
+    });
+    
+    // Programmatically click the anchor element to start the download
+    anchor.click();
+    
+    // Clean up by revoking the object URL
+    URL.revokeObjectURL(anchor.href);
+
+    // reset unsaved changes
+    setUnsavedChanges(false)
+  };
 
   /** Undo Redo Functionalities **/
   const handleUndo = () => {
+    console.log("undo")
     editor.commands.undo();
   };
 
   const handleRedo = () => {
+    console.log("redo")
     editor.commands.redo();
   };
 
@@ -76,36 +152,53 @@ export default function Menubar() {
     document.body.style.zoom = zoomLevel+'%';
   }, [zoomLevel])
 
+  const handleChange = () => {
+    // Set unsavedChanges to true when changes are made
+    setUnsavedChanges(true);
+  };
+
+  // Attach handleChange to editor's change event
+  editor.on('transaction', () => {
+    handleChange();
+  });
+
+
 
   if (!editor) {
     return null;
   }
   return (
     <div className="container menubar-container" >
-      <button className="menubar-button">
+      <button className="menubar-button" onClick = {handleNewFile}>
         <NoteAddIcon />
         <span className="menubar-button-label">New</span>
       </button>
-      <button className="menubar-button">
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        onChange={openFileExplorer}
+      />
+      <button className="menubar-button" onClick={handleOpenFile}>
         <NoteAddIcon className="menubar-button-icon"/>
         <span className="menubar-button-label">Open</span>
       </button>
-      <button className="menubar-button">
+      <button className="menubar-button" disabled={!unsavedChanges}>
         <SaveIcon className="menubar-button-icon"/>
         <span className="menubar-button-label">Save</span>
       </button>
-      <button className="menubar-button">
+      <button className="menubar-button" onClick={handleSaveAs} disabled={!unsavedChanges}>
         <SaveAsIcon className="menubar-button-icon"/>
         <span className="menubar-button-label">Save As</span>
       </button>
       <div className="vertical-division"/>
-      <button id="MENU-UNDO" className="menubar-button">
+      <button id="MENU-UNDO" className="menubar-button" onClick={handleUndo} disabled={!editor.can().undo()}>
         <UndoIcon className="menubar-button-icon"/>
-        <span className="menubar-button-label" onClick={handleUndo}>Undo</span>
+        <span className="menubar-button-label" >Undo</span>
       </button>
-      <button id="MENU-REDO" className="menubar-button">
-        <RedoIcon className="menubar-button-icon"/>
-        <span className="menubar-button-label" onClick={handleRedo}>Redo</span>
+      <button id="MENU-REDO" className="menubar-button" disabled={!editor.can().redo()} onClick={handleRedo}>
+        <RedoIcon className="menubar-button-icon" />
+        <span className="menubar-button-label">Redo</span>
       </button>
       <button id="MENU-CUT" className="menubar-button" onClick={handleCut}>
         <ContentCutIcon className="menubar-button-icon"/>
