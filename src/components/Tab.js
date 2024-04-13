dd56d483ad4418d1f4bab517ea7a2562a2fe75c5
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-04-13 21:44:22 +0800

    detect duplicate name

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,21 +1,34 @@
 import CloseIcon from '@mui/icons-material/Close';
 import AddIcon from '@mui/icons-material/Add';
 import { useState, useEffect } from 'react';
-import { cleanInputForId } from "../utils"
+import { cleanInputForId, getLocalStorageItem, LOCAL_STORAGE_KEYS } from "../utils"
+
 export default function Tab({ name, isSelected, onTabDelete, onTabChangeName, onTabClick, hasUnsavedChanges }) {
   const [tabName, setTabName] = useState(name);
+  const [error, setError] = useState(false);
   const handleChange = (e) => {
     setTabName(e.target.value);
   };
 
   const handleBlur = () => {
-    onTabChangeName(tabName);
+    var tabs = getLocalStorageItem(LOCAL_STORAGE_KEYS.FILE_LIST)
+    if(!tabs.some(tab => tab.name === tabName)){
+      onTabChangeName(tabName);
+      setError(false)
+    } else {
+      setError(true)
+      setTimeout(() => {
+        setTabName(name)
+        setError(false)
+      }, 1500)
+    }
   };
 
 
   return (
     <div className={isSelected ? "tab-container-active" : "tab-container-inactive"}>
-      <div className="tab-name-cont "onClick={onTabClick} id={"TAB-CONTAINER-"+cleanInputForId(tabName)}>
+      <div className="tab-name-cont " onClick={onTabClick} id={"TAB-CONTAINER-" + cleanInputForId(tabName)}>
+      {error && <span className='tab-name-error'>Name already used.</span>}
         <input
           id={"TAB-INPUT-"+cleanInputForId(tabName)}
           value = {tabName}
