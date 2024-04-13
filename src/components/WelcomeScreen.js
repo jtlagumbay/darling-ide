94cb06dd520764acc84599acd6cdae6b5e7bedc3
efypuyot <92715199+efypuyot@users.noreply.github.com>
2024-04-13 20:27:52 +0800

    update welcomescreen bgcolor

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -4,7 +4,7 @@ import WelcomeScreenGif from '../images/Welcome Screen.gif';
 
 export default function WelcomeScreen() {
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#DDA15E' }}>
         <img src={WelcomeScreenGif} style={{ width: '100%', height: 'auto', maxWidth: '100%' }} alt="Welcome Screen" />
         </div>
     );
