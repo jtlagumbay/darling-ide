619fe67fac3128ed9dd198c27941b6dd3bba27cd
Princess Ethel <pmatillo@up.edu.ph>
2024-04-15 20:31:34 +0800

    add guide comments for `app`, `commands`, and `guide`

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// imports
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import Guide from './components/Guide';
@@ -8,38 +9,54 @@ import WelcomeScreen from './components/WelcomeScreen';
 import HeaderImg from './images/header.png';
 import { LOCAL_STORAGE_KEYS, getLocalStorageItem, setLocalStorageItem } from './utils';
 
+/**
+ * 
+ * @returns Main component of the application
+ */
 function App() {
-  const [transcript, setTranscript] = useState('');
-
+  // a react hook component to set the initial file content and file list
   useEffect(() => {
+    // fetch the stored file content
     const storedFileContent = getLocalStorageItem(LOCAL_STORAGE_KEYS.FILE_CONTENT);
+
+    // if there is a stored file content, set it to the initial content
     if (storedFileContent) {
       setLocalStorageItem(LOCAL_STORAGE_KEYS.FILE_INITIAL_CONTENT, storedFileContent)
     }
 
+    // fetch the stored file list
     const fileList = getLocalStorageItem(LOCAL_STORAGE_KEYS.FILE_LIST)
+    
+    // if there is no file list, create an empty file list 
     if (!fileList) {
       setLocalStorageItem(LOCAL_STORAGE_KEYS.FILE_LIST, [])
     }
   }, []);
   
+  // initialize welcome screen component
   const [showWelcomeScreen, setShowWelcomeScreen] = useState(true);
 
+  // make the welcome screen component appear for 19 seconds
   useEffect(() => {
+    // after 19 seconds, record locally that welcome screen has been shown 
+    // to avoid showing it again when the user refreshes
     const timer = setTimeout(() => {
       setShowWelcomeScreen(false);
       localStorage.setItem('showWelcomeScreen', 'true');
     }, 19000); // 19 seconds
 
-    return () => clearTimeout(timer); 
+    return () => clearTimeout(timer);
+    // call this hook every time showWelcomeScreen changes value 
   }, [showWelcomeScreen]);
 
+  // if the welcome screen has not been shown, display the welcome screen
   if (localStorage.getItem('showWelcomeScreen') === null){
     return <WelcomeScreen />;
   }
 
   return (
     <div className="App">
+      {/* holds the left components of the screen */}
       <div className='left-component'>
         {/* header */}
         <img src={HeaderImg} alt="darling" className='logo' />
@@ -48,7 +65,7 @@ function App() {
       </div>
         
 
-        {/* content */}
+        {/* holds the right ccomponent of the screen */}
         <div className='right-component'>
           <Header />
           <TextEditor />
--- src/components/Guide.js
+++ src/components/Guide.js
@@ -1,3 +1,8 @@
+
+/**
+ * 
+ * @returns Displays the guide at the left side of the screen (static only)
+ */
 export default function Guide() {
     return(
         <div className='guide'>
@@ -7,17 +12,25 @@ export default function Guide() {
           {/* guide content */}
           <div className='guide-content'>
             <p className='note'> Note: Always say <strong>"please."</strong> at the end of the voiced instruction because it serves as the <strong>ENTER</strong> key.</p>
+            
+            {/* Guide for file handling commands */}
             <p>When opening a file, you need to say, <strong>“Honey, new file, please.”</strong></p>
             <p>When opening an existing file, you need to say, <strong>“Honey, open file, please.”</strong></p>
-            <p>When saving a file, you need to say, <strong>“Honey, save the file, please.”</strong></p>
+            <p>When saving a file, you need to say, <strong>“Honey, save file, please.”</strong></p>
             <p>When saving as a new file, you need to say, <strong>“Honey, save as new file, please.”</strong></p>
+            
+            {/* Guide for text editing */}
             <p>When undoing, you need to say, <strong>“Honey, undo, please."</strong></p>
             <p>When redoing, you need to say, <strong>“Honey, redo, please."</strong></p>
             <p>When cutting, you need to say, <strong>“Honey, cut, please."</strong></p>
             <p>When pasting, you need to say, <strong>“Honey, paste, please."</strong></p>
+            <p>When typing, you need to say, <strong>“Honey, type this: (the text to type), please."</strong></p>
+
+            {/* Guide for display */}
             <p>When zooming out, you need to say, <strong>“Honey, zoom out, please."</strong></p>
             <p>When zooming in, you need to say, <strong>“Honey, zoom in, please."</strong></p>
-            <p>When closing, you need to say, <strong>“Honey, close, please."</strong></p>
+            
+            {/* Guide for text manipulation (toolbar) */}
             <p>When selecting all, you need to say, <strong>“Honey, select all, please."</strong></p>
             <p>When deselecting all, you need to say, <strong>“Honey, deselect all, please."</strong></p>
             <p>When making text bold, you need to say, <strong>“Honey, bold, please."</strong></p>
