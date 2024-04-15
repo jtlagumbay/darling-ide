a7542f31f9d65d135e05c6294fc5899d41eea2ae
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2024-04-15 21:05:34 +0800

    saveTab before downloading

--- src/components/Menubar.js
+++ src/components/Menubar.js
@@ -70,6 +70,7 @@ export default function Menubar({ onTabAdd, onTabSave,
   }
 
   const handleConfirmDownload = () => {
+    onTabSave()
     saveFile();
     setIsDownloadModalOpen(false);
     setIsDownloadConfirm(false);
