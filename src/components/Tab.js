4128c749e91c2c2b5edcbd3f50800ad358035cae
Jade Zahyen Cataques <109787941+dev-jadezahyen@users.noreply.github.com>
2024-04-15 23:15:46 +0800

    updated tab component

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -2,24 +2,10 @@ import CloseIcon from '@mui/icons-material/Close';
 import AddIcon from '@mui/icons-material/Add';
 import { useState, useEffect } from 'react';
 import { cleanInputForId, getLocalStorageItem, LOCAL_STORAGE_KEYS } from "../utils"
-import Modal from './Modal/Modal';
-import './Modal/Modal.css';
 
-/**
- * This component represents a single tab in a tabbed interface.
- * @param {*} name - the name of the tab
- * @param {*} isSelected - whether the tab is currently selected or not
- * @param {*} onTabDelete - function to call when the tab is deleted
- * @param {*} onTabChangeName - function to call when the tab name is changed
- * @param {*} onTabClick - function to call when the tab is clicked
- * @param {*} hasUnsavedChanges - whether the tab has unsaved changes
- * @param {*} id - the unique identifier of the tab
- * @returns tab component
- */
 export default function Tab({ name, isSelected, onTabDelete, onTabChangeName, onTabClick, hasUnsavedChanges, id}) {
   const [tabName, setTabName] = useState(name);
   const [error, setError] = useState(false);
-  const [isClosedModalOpen, setIsClosedModalOpen] = useState(false);
 
   /* This function handles renaming of the tab. */
   const handleChange = (e) => {
@@ -47,37 +33,13 @@ export default function Tab({ name, isSelected, onTabDelete, onTabChangeName, on
     }
   };
 
-  /**
-   * This function handles the deletion of a tab.
-   * If the tab has unsaved changes, it opens a confirmation modal.
-   * Otherwise, it calls the onTabDelete callback directly.
-   */
-  const handleTabDelete = () => {
-    if(hasUnsavedChanges){
-      setIsClosedModalOpen(true);
-    } else {
-      onTabDelete();
-    }
-  }
-
-  /**
-   * This function handles the confirmation of tab deletion in the modal.
-   * It calls the onTabDelete callback and then closes the modal.
-   */
-  const handleCloseModalConfirm = () => {
-    onTabDelete();
-    setIsClosedModalOpen(false);
-  }
-
   return (
-    /**
+     /**
      * The div's class depends whether the tab is selected or not.
      * The tab name input field is rendered as read-only if the tab is not selected.
      * Error message is displayed if the tab name is already used.
      * The tab close container is rendered with a close icon.
-     * When the close icon is clicked, the handleTabDelete function is called.
-     * If there are unsaved changes, a modal is opened to confirm the tab closure.
-     * The modal's confirm action is handled by the handleCloseModalConfirm function.
+     * When the close icon is clicked, the onTabDelete prop is called.
      */
     <div className={isSelected ? "tab-container-active" : "tab-container-inactive"}>
       <div className="tab-name-cont " onClick={onTabClick} id={"TAB-CONTAINER-" + cleanInputForId(tabName)}>
@@ -92,22 +54,10 @@ export default function Tab({ name, isSelected, onTabDelete, onTabChangeName, on
           className={`${isSelected ? "tab-name-input-editable" : "tab-name-input-readonly"} ${hasUnsavedChanges ? "italicize" : ""}`}
         />
       </div>
-      <div className="tab-close-cont" onClick={handleTabDelete} id={"TAB-CLOSE-"+cleanInputForId(tabName)}>
+      <div className="tab-close-cont" onClick={onTabDelete} id={"TAB-CLOSE-"+cleanInputForId(tabName)}>
         <CloseIcon className="tab-close" fontSize='small' />
         <span className="tab-close-label">Close</span>
       </div>
-      
-      {/* Modal for Closing the Tab with Unsaved Changes */}
-      <Modal 
-        isOpen={isClosedModalOpen}
-        onClose={() => setIsClosedModalOpen(false)}
-        headerText="Unsaved Changes"
-        content="There are unsaved changes. Are you sure you want to close the tab?"
-        buttonText1="No"
-        buttonText2="Yes"
-        onCancel={() => setIsClosedModalOpen(false)}
-        onConfirm={handleCloseModalConfirm}
-      />
     </div>
   )
 }
